0d17cc152a46817eaf7e3c1fca2972f2e5ecfb43
vqton <vuquangton@outlook.com>
2024-07-29 08:35:14 +0700

    inint

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Pug Example', message: 'Hello, Pug!' });
-});
-
-module.exports = router;
